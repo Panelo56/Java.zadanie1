52872beae9cd0759cdd571f8f064d7146d1ab056
Kirill Khrenov <metainf12@mail.ru>
2024-06-28 11:00:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 15000;
+        int b = 20;
+        int c = a / b;
+        System.out.print("Клиент получит " + c + " бонусных миль.");
+    }
+}
